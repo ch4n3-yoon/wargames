0f52f23daedcbd83159c116910f2ed0773231381
chaneyoon <chaneyoon@gmail.com>
2017-08-23 11:13:22 +0900

    add write blog url

--- hackburger.ee/Enter password to get candy/getFlag.py
+++ hackburger.ee/Enter password to get candy/getFlag.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 # coding: utf-8
 
+
+"""
+
+write up : http://chaneyoon.tistory.com/207
+
+"""
+
+
 import requests
 
 url = "http://burger.laboratorium.ee:8003/index.php"
@@ -13,4 +21,4 @@ content = requests.post(url, data=data).content
 
 
 
-print "\n", content
+print "\n", content, "\n"
