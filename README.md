2de9ba15b6baa834353db8f63f6dff9c230306af
chaneyoon <chaneyoon@gmail.com>
2017-08-24 09:23:14 +0900

    fix README.md

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # wargames
-내가 워게임 문제를 풀면서 쓴 스크립트들
+The scripts that I made for solving wargames
 
 made by ch4n3
+
+## Write-Up Lists
+ - hackburger.ee
+ - wargame.kr
+ - ch4n3.dothome.co.kr
--- wargame.kr/php_c?.py
+++
@@ -1,8 +0,0 @@
-# -*- coding : utf-8 -*-
-import requests
-
-data = {'d1': 2147483647, 'd2': -2147483644}
-url = 'http://wargame.kr:8080/php_c/'
-
-r = requests.post(url, data=data)
-print(r.text[0:40])
